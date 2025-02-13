e3df9cab40f474dc19b87eedf8b5a1b0c949d4e7
Man-Of-May <bmanomay2018@gmail.com>
2025-02-13 08:30:34 +0000

    More things

--- 02_Basics&AdvanceDT/01_Arrays.js
+++ 02_Basics&AdvanceDT/01_Arrays.js
@@ -1,4 +1,7 @@
-const myArr = [0, 1, 2, 3, 4, 5, true, hello]
+const myAr = [0, 1, 2, 3, 4, 5, true, hello]
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
 
 // Can contain a mix of different datatypes, not associative arrays by which I mean 
 // In summary, JavaScript arrays are not associative because they use integer indices, 
@@ -6,3 +9,5 @@ const myArr = [0, 1, 2, 3, 4, 5, true, hello]
 // In JavaScript, you would use objects or Map for associative behavior.
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+const myArr2 = new Array(1,2,3,4)
