ae2c87480cbdc1b1bddc3c3375c2102e32ec8145
Man-Of-May <bmanomay2018@gmail.com>
2025-02-13 08:20:44 +0000

    Primitive DT complete

---
+++ 02_Basics&AdvanceDT/01_Arrays.js
@@ -0,0 +1,8 @@
+const myArr = [0, 1, 2, 3, 4, 5, true, hello]
+
+// Can contain a mix of different datatypes, not associative arrays by which I mean 
+// In summary, JavaScript arrays are not associative because they use integer indices, 
+// while languages like PHP support associative arrays with string keys. 
+// In JavaScript, you would use objects or Map for associative behavior.
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
