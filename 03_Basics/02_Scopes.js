2c5b74630d43eb4d9d2186a87f99912967a9bf40
Man-Of-May <bmanomay2018@gmail.com>
2025-02-22 05:28:12 +0000

    almost

--- 03_Basics/02_Scopes.js
+++ 03_Basics/02_Scopes.js
@@ -23,3 +23,60 @@ if (true) {
 => Global scope is different for node environment and browser
 
 */
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+/* 
+
+=> Kids can ask ice cream from elder but it is awekword if elder ask the ice cream from kids.
+=> It means, inner function can access the variable from outside but outside function cant ask 
+    inner member variables etc.
+=> Variables declared inside a function cannot be accessed outside the function.
+
+=> Every time function is called, then every time it is kept in stack it also partially defined as closure
+    Closure in simple line, the inner function can access the outside declared variable
+*/
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+// There are 2 technique to create a function, using bassic function, other is function expression
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5) // Due to concept of hoisting this way of function declaration will give error
+const addTwo = function(num){
+    return num + 2
+}
+
+// Hositing is not possible with function expression, but hoisting is possible with basic function only
