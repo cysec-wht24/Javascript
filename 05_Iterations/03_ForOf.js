fe9001ba9865d49fad749d9bb09a3132863cf329
Man-Of-May <bmanomay2018@gmail.com>
2025-02-23 20:57:05 +0000

    more

--- 05_Iterations/03_ForOf.js
+++ 05_Iterations/03_ForOf.js
@@ -27,3 +27,58 @@ const greetings = "Hello world!"
 for (const greet of greetings) {
     //console.log(`Each char is ${greet}`)
 }
+
+// Maps
+
+/* 
+    Map is use to store key, value pair
+
+    Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+
+    Map is like object with slight difference
+
+    Map is used to store unique values, persist the same order how it entered
+
+    We can iterate the Map using for...of loop by destructuring the value
+
+  for(const [key, value] of countries){
+      console.log(`Key: ${key} Value: ${value}`)
+  }
+
+    Note: Not possible to iterate the object using for..of
+
+*/
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+// console.log(map);
+
+/* for (const key of map) {
+    console.log(key); // Eh, prints the whole array we don't want that, we want indivisual values
+} */
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}// maps are iterable through for of loop
+
+/* const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+    OR 
+
+    const myObject = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+}
+In Both of them myObject is not iterable, thus maps are iterable by this way of for loop 
+    but not in case of objects, there are other ways of iterating objects
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }  
+// */
