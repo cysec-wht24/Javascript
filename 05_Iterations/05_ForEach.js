907aebe1e575691ddf8b6de79be2c73ab5920bf3
Man-Of-May <bmanomay2018@gmail.com>
2025-02-27 08:33:15 +0000

    damn

--- 05_Iterations/05_ForEach.js
+++ 05_Iterations/05_ForEach.js
@@ -42,3 +42,20 @@ myCoding.forEach( (item) => {
     
     console.log(item.languageName);
 } )
+
+const numbers = [10, 20, 30];
+
+// Logging the element, its index, and the entire array
+numbers.forEach((number, index, array) => {
+    console.log(`Element: ${number}, Index: ${index}, Array: ${array}`);
+});
+
+/* 
+=> For most of the time, we may use forEach loop for array
+=> Array has some inbuilt methods, here forEach method, so we can use it to access the elements of an array
+=> Passing callback means give me the function to operate
+=> callback function doesn't have name
+=> While iterating an array, every time the function will get executed, and take as a paramater to it.
+=> forEach doesnt only return item, but it will return item, index and whole array
+=> callback has access to all the element of an array
+ */
